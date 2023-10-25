aeb9afadc2a0a73a2a7ee679e9b18795890b1513
besart <besarto77@gmail.com>
2023-10-24 17:08:29 -0700

    Delete PP LLM directory

--- PP LLM/scikit-llm/skllm/__init__.py
+++
@@ -1,7 +0,0 @@
-# ordering is important here to prevent circular imports
-from skllm.models.gpt.gpt_zero_shot_clf import (
-    MultiLabelZeroShotGPTClassifier,
-    ZeroShotGPTClassifier,
-)
-from skllm.models.gpt.gpt_few_shot_clf import FewShotGPTClassifier
-from skllm.models.gpt.gpt_dyn_few_shot_clf import DynamicFewShotGPTClassifier
